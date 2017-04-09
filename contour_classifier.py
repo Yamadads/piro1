9bc535e8a6a1fc86646118eb1c97868e267a7f3a
uHappyLogic <kosiak.lukasz@gmail.com>
2017-04-10 00:00:13 +0200

    extracting of sorted junction contour points added

--- contour_classifier.py
+++ contour_classifier.py
@@ -1,14 +1,25 @@
 import image_utils as iu
 import cv2
+import numpy as np
+
 
 def get_classification(normalized_figures):
     classification = []
-    get_contour(normalized_figures[0])
+    print get_junction_contour(normalized_figures[0])
+
     for i in range(len(normalized_figures)):
         classification.append(i)
+
     return classification
 
-def get_contour(figure):
-    cont = iu.get_contour(figure)
-    cont = cv2.approxPolyDP(cont, 2,True)
-    print (cont)
+
+def get_junction_contour(figure):
+
+    contour = cv2.approxPolyDP(iu.get_contour(figure), 2, True)
+
+    max_y = max(contour[:, 0, 1])
+    threshold = 5
+
+    result = sorted([r.tolist() for r in contour[:,0,:] if r[1] < max_y - threshold], key=lambda idx: idx[0])
+
+    return result
