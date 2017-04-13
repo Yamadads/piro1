cbc1b5c8af48ca4cd00b7d0c74718813657333bf
uHappyLogic <kosiak.lukasz@gmail.com>
2017-04-14 00:36:53 +0200

    huClassifier added

--- contour_classifier.py
+++ contour_classifier.py
@@ -5,12 +5,88 @@ import numpy as np
 
 def get_classification(normalized_figures):
     classification = []
-    print get_junction_contour(normalized_figures[0])
 
-    for i in range(len(normalized_figures)):
-        classification.append(i)
+    test(normalized_figures)
+    return
 
-    return classification
+    hu_moments = [[], []]
+
+    for image_i in range(0, len(normalized_figures)):
+        junction0 = extract_junction_image(normalized_figures[image_i])
+        insert_hu_moments(junction0, hu_moments, image_i)
+
+        junction_1 = iu.rotate_bound(np.invert(junction0), 180)
+        ret, junction_1 = cv2.threshold(junction_1, 127, 255, 0)
+        insert_hu_moments(junction_1, hu_moments, image_i)
+
+    hu_moments[0].sort(key=lambda x:x[0])
+    hu_moments[1].sort(key=lambda x:x[0])
+
+    dist = [[],[]]
+
+    for i_m in range(0, 1):
+        dist[i_m] = [[hu_moments[i_m][i + 1][0] - hu_moments[i_m][i][0], [hu_moments[i_m][i][1], hu_moments[i_m][i + 1][1]]] for i in range(0, len(hu_moments[i_m]) - 1)]
+        dist[i_m].sort(key=lambda x:x[0])
+
+    print dist[0]
+
+    print hu_moments[0]
+    #print hu_moments[1]
+
+    return
+
+
+def test(normalized_figures):
+
+    img_test0 = normalized_figures[0]
+    img_test1 = normalized_figures[5]
+
+    junction0 = extract_junction_image(img_test0)
+    junction1 = extract_junction_image(img_test1)
+
+    # iu.show_image('junction1', junction1)
+    rij1 = iu.rotate_bound(np.invert(junction1), 180)
+
+    # print cv2.HuMoments(cv2.moments(rij1)).flatten()
+    # iu.show_image('inverted rotaded j5', rij1)
+
+
+    ret, tresh_junction_1 = cv2.threshold(rij1, 127, 255, 0)
+    hu_jun_1 = cv2.HuMoments(cv2.moments(tresh_junction_1)).flatten()
+
+    hu_jun_0 = cv2.HuMoments(cv2.moments(junction0)).flatten()
+
+    print hu_jun_0
+    print hu_jun_1
+
+    print ('============')
+    print -np.sign(hu_jun_1) * np.log10(np.abs(hu_jun_1))
+    print -np.sign(hu_jun_0) * np.log10(np.abs(hu_jun_0))
+
+    return
+
+
+def insert_hu_moments(image, hu_list, id):
+    hu_jun_0 = cv2.HuMoments(cv2.moments(image)).flatten()
+
+    log_hu_jun_0 = hu_jun_0 # -np.sign(hu_jun_0) * np.log10(np.abs(hu_jun_0))
+
+    hu_list[0].append([log_hu_jun_0[0], id])
+    hu_list[1].append([log_hu_jun_0[1], id])
+
+
+def extract_junction_image(image):
+    contour = get_junction_contour(image)
+
+    max_y = max(contour, key=lambda x:x[1])[1]
+
+    return image[:][:max_y]
+
+    # slice = [arr[i][0:2] for i in range(0, 2)]
+
+    # new_image = [image[i][0:max_y] for i in range(0, len(image))]
+    # iu.show_image('dsd', image)
+    # iu.show_image('dsd', new_image)
 
 
 def get_junction_contour(figure):
@@ -20,6 +96,6 @@ def get_junction_contour(figure):
     max_y = max(contour[:, 0, 1])
     threshold = 5
 
-    result = sorted([r.tolist() for r in contour[:,0,:] if r[1] < max_y - threshold], key=lambda idx: idx[0])
+    result = sorted([r.tolist() for r in contour[:, 0, :] if r[1] < max_y - threshold], key=lambda idx: idx[0])
 
     return result
