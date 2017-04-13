87b8616590a6d1650615d0120f30183363d0f3e7
Yamadads <pawlaksebastian@hotmail.com>
2017-04-13 23:07:44 +0200

    simple classificator

--- contour_classifier.py
+++ contour_classifier.py
@@ -1,15 +1,24 @@
 import image_utils as iu
 import cv2
 import numpy as np
+import operator
+
+vertices = {}
 
 
 def get_classification(normalized_figures):
-    classification = []
-    print get_junction_contour(normalized_figures[0])
+    classification = {}
+    for i in range(len(normalized_figures)):
+        contour = get_junction_contour(normalized_figures[i])
+        vertices[i]=(len(contour))
 
     for i in range(len(normalized_figures)):
-        classification.append(i)
-
+        x = {}
+        for j in range(len(normalized_figures)):
+            if i!=j:
+                x[j]=abs(vertices[i]-vertices[j])
+        sorted_x=sorted(x.items(),key=lambda x: x[1])
+        classification[i] = [l[0] for l in sorted_x]
     return classification
 
 
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 program.py $@
+python program.py $@
